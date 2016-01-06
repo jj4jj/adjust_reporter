7769b106cd473aa26506d78336cf544840f619ac
jj4jj <resc@vip.qq.com>
2016-01-06 13:47:46 +0800

    add cp

--- adjust_report.cpp
+++ adjust_report.cpp
@@ -8,22 +8,43 @@ typedef void(*adjust_on_report_t)(void *ud, int id, const char * json);
 
 struct  adjust_report_t {
     std::string key;
+    std::string event_url;
+    std::string revenue_url;
     int                 max_threads;
     mmqueue_t *         mmq;
     adjust_on_report_t  cb;
     void *              cb_ud;
     CURL            * * curls;
+    int                 seqid;
 } G;
 
+struct adjust_work_input_t {
+    int         seqid;
+    char        url[1];
+};
+struct adjust_work_output_t {
+    int         seqid;
+    char        data[1];
+};
+static size_t write_to_output(char * data, size_t size, size_t nmemb, void *output){
+    memcpy(output, data, size*nmemb);
+}
 static void _adjust_report(void * ctx, int idx, char * omsg, size_t * omsg_szp, 
                             const char * inmsg, size_t inmsg_sz){
     CURL *curl = G.curls[idx];
-    //get url
-    //curl_easy_setopt(curl, CURLOPT_URL, url);
-    // curl_easy_perform(curl); /* ignores error */
-    //http://s2s.adjust.com/revenue  (revenue events)
-    //const char * report_url;
-
+    adjust_work_input_t * input = (adjust_work_input_t*)inmsg;
+    adjust_work_output_t * output = (adjust_work_output_t*)omsg;
+    output->seqid = input->seqid;
+    output->data[0] = 0;
+    /* write to this file */
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, output->data);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_output);
+    curl_easy_setopt(curl, CURLOPT_URL, input->url);
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK){
+        //error
+        return;
+    }
 }
 
 int     adjust_init(adjust_config_t & conf){
@@ -47,6 +68,7 @@ int     adjust_init(adjust_config_t & conf){
     if (G.mmq){
         return 0;
     }
+    G.seqid = 1;
     return -1;
 }
 void    adjust_set_report_cb(adjust_on_report_t cb, void * ud){
@@ -61,15 +83,133 @@ int     adjust_poll(int maxproc = 200){
             return maxproc;
         }
         if (G.cb){
-#warning "todo get id"
-            G.cb(G.cb_ud, 0, _msg_buffer);
+            adjust_work_output_t * p = (adjust_work_output_t*)_msg_buffer;
+            G.cb(G.cb_ud, p->seqid, p->data);
         }
     }
     return 0;
 }
-int     adjust_report(const char * url, const char * json){
+static const char * _make_url_param(char * buff, int buffsz, ...){
+    CURL *curl = curl_easy_init();
+    if (!curl) {
+        return nullptr;
+    }
+
+    va_list ap;
+    va_start(ap, buffsz);
+    const char * pkey = va_arg(ap, const char *);
+    const char * pval = va_arg(ap, const char *);
+    int c = strlen(buff);
+    while (pkey && pval){
+        char *output = curl_easy_escape(curl, pkey, strlen(pkey));
+        if (!output) {
+            curl_easy_cleanup(curl);
+            return nullptr;
+        }
+        strncpy(buff + c, output, buffsz - c - 1);
+        c += strlen(output);
+        curl_free(output);
+        buff[c] = '=';
+        c++;
+        output = curl_easy_escape(curl, pval, strlen(pval));
+        if (!output) {
+            curl_easy_cleanup(curl);
+            return nullptr;
+        }
+        strncpy(buff + c, output, buffsz - c - 1);
+        c += strlen(output);
+        curl_free(output);
+        buff[c] = 0;
+        pkey = va_arg(ap, const char *);
+        if (pkey){
+            pval = va_arg(ap, const char *);
+        }
+        else {
+            pval = nullptr;
+        }
+    }
+    va_end(ap);
+CLEAN_UP:
+    curl_easy_cleanup(curl);
+    return buff;
+}
+int     adjust_report(const adjust_event_t * evt){
 #warning  "todo"
-    return 0;
+    static char input_msg_buff[4096];
+    adjust_work_input_t * input = (adjust_work_input_t *)input_msg_buff;
+    if (G.seqid <= 0){
+        G.seqid = 1;
+    }
+    int seqid = G.seqid;
+    input->seqid = seqid;
+    input->url[0] = 0;
+    if (evt->type == adjust_event_t::REVENUE){
+        strcpy(input->url, G.revenue_url.c_str());
+        strcat(input->url, "?");
+    }
+    else {
+        strncpy(input_msg_buff, G.event_url.c_str());
+        strcat(input->url, "?");
+    }
+
+    char bufftime[32];
+    time_t tn = time(NULL);
+    struct tm _tmp;
+    struct tm * ntm = localtime_r(&tn, &_tmp);
+    const char * udidk = "idfa";
+    const char * udidv = "";
+    switch (evt->devid.type){
+    case adjust_event_t::adjust_event_udid_t::IDFA:
+        udidk = "idfa";
+        udidv = evt->devid.udid.idfa;
+        break;
+    case adjust_event_t::adjust_event_udid_t::IDFV:
+        udidk = "idfv";
+        udidv = evt->devid.udid.idfv;
+        break;
+    case adjust_event_t::adjust_event_udid_t::MAC:
+        udidk = "mac";
+        udidv = evt->devid.udid.mac;
+        break;
+    case adjust_event_t::adjust_event_udid_t::MAC_MD5:
+        udidk = "mac_md5";
+        udidv = evt->devid.udid.mac_md5;
+        break;
+    case adjust_event_t::adjust_event_udid_t::MAC_SHA1:
+        udidk = "mac_sha1";
+        udidv = evt->devid.udid.mac_sha1;
+        break;
+    case adjust_event_t::adjust_event_udid_t::ANDROID_ID:
+        udidk = "android_id";
+        udidv = evt->devid.udid.android_id;
+        break;
+    case adjust_event_t::adjust_event_udid_t::GPS_ADID:
+        udidk = "gps_adid";
+        udidv = evt->devid.udid.gps_adid;
+        break;
+    }
+    strftime(bufftime, sizeof(bufftime), "%FT%X%z", ntm);
+    const char * param = _make_url_param(input->url, sizeof(input_msg_buff) - sizeof(adjust_work_input_t),
+        "app_token", evt->base.app_token, 
+        "event_token", evt->base.event_token,
+        "s2s", 1,
+        "created_at", bufftime,
+        udidk, udidv,
+        nullptr);
+    
+    if (evt->type == adjust_event_t::REVENUE){
+        param = _make_url_param(input->url, sizeof(input_msg_buff)-sizeof(adjust_work_input_t),
+            "revenue", evt->revenue.revenue,
+            "currency", evt->revenue.currency,
+            "environment", evt->revenue.environment,
+            nullptr);
+    }
+    int ret = mmqueue_put(G.mmq, input_msg_buff, sizeof(adjust_work_input_t) + strlen(input->url));
+    if (ret){
+        return -1;
+    }
+    ++G.seqid;
+    return seqid;
 }
 int     adjust_destroy(){
     if (G.mmq){
--- adjust_report.h
+++ adjust_report.h
@@ -4,16 +4,46 @@ struct adjust_config_t {
     const char * sharekey;
     int          max_thread;
     int          max_queue_buff_size;
+    const char * report_revenue_url;
+    const char * reprot_event_url;
 };
 
 typedef void(*adjust_on_report_t)(void *ud, int id, const char * json);
-//http://s2s.adjust.com/revenue  (revenue events)
-//const char * report_url;
+
+struct adjust_event_t {
+    enum adjust_event_type {
+        EVENT, REVENUE
+    } type;
+    struct {
+        const char *app_token;//adjust app token from dashboard;     4w565xzmb54d	来自控制面板的 adjust 应用识别码
+        const char *event_token;//adjust event token from dashboard;	f0ob4r	来自控制面板的 adjust 事件识别码
+    }  base; //requried
+    struct adjust_event_udid_t{
+        enum divid_type {
+            IDFA,IDFV,MAC,MAC_MD5,MAC_SHA1,ANDROID_ID,GPS_ADID
+        } type;
+        union {
+            const char * idfa;//iOS ID for Advertisers	D2CADB5F - 410F - 4963 - AC0C - 2A78534BDF1E	广告商的 iOS ID	带 “ - “
+            const char * idfv;//iOS ID for Vendors	EF76726C - D952 - 451C - 8E1A - 4E86938BDC20	卖家的 iOS ID	带 “ - “
+            const char * mac;//	15118fdce61d	设备的 MAC 地址（仅 Android）	短，不带 “ : ”
+            const char * mac_md5;//MAC address of device (Android only)	e3f5536a141811db40efd6400f1d0a4e	MAC 的 MD5（仅 Android）	大写字母，不带 “ : ”
+            const char * mac_sha1;//MD5 of MAC (Android only)	c1976429369bfe063ed8b3409db7c7e7d87196d9	MAC 的 SHA1（仅 Android）	大写字母，带 “ : ”
+            const char * android_id;//SHA1 of MAC (Android only)	e1cbfb61613b4f50	Android ID
+            const char * gps_adid;//Android ID	660e1d86 - 6796 - 463a - be86 - 897993136018	Google Play 广告商 ID	带 “ - “
+        } udid;
+    } devid;
+    struct {
+        const char *revenue;//Value of revenue events, in full currency units 	收入事件的值，采用完整货币单位(150 = $ 150)
+        const char *currency;//Currency code of revenue event, see note below; ISO 4217, e.g. USD or EUR	收入事件货币代码，参见以下注释；ISO 4217，例如USD或EUR
+        const char *environment;//Environment to post the data to, “sandbox” or “production”.	发布数据的环境， “sandbox” 或 “production”
+    } revenue; //optional when type is revenue
+};
+
 
 int     adjust_init(adjust_config_t & conf);
 void    adjust_set_report_cb(adjust_on_report_t cb, void * ud);
 int     adjust_poll(int maxproc = 200);
-int     adjust_report(const char * url, const char * json);
+int     adjust_report(const adjust_event_t * evt);
 int     adjust_destroy();
 
 
